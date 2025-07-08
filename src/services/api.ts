a71307b15bd41a05c7a1769606130ce735b1e623
Iago Diniz <iagoo_ptu@hotmail.com>
2025-07-08 15:24:10 -0300

    fix:corrigindo token inválido

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,6 @@
+import { logout } from "@utils/auth";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:3333",
@@ -12,5 +14,17 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const navigate = useNavigate();
+      logout(); // Remove token
+      navigate("/login"); // Redireciona para login
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
