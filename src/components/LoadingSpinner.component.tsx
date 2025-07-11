41cbffdcdad1af082e77819ca55ebad8cfe528ee
Iago Diniz <iagoo_ptu@hotmail.com>
2025-07-11 07:24:30 -0300

    style:mudando loading

--- src/components/LoadingSpinner.component.tsx
+++ src/components/LoadingSpinner.component.tsx
@@ -1,5 +1,5 @@
 import * as S from "@styles/components/LoadingSpinner.styles";
-
+import LogoImage from "@assets/logo32.png";
 interface Props {
   loading: boolean;
   size?: number;
@@ -8,7 +8,7 @@ interface Props {
 export const LoadingSpinner: React.FC<Props> = ({ loading, size }) => {
   return loading ? (
     <S.SpinnerContainer>
-      <S.Spinner size={size} />
+      <S.SpinningLogo src={LogoImage} />
     </S.SpinnerContainer>
   ) : null;
 };
--- src/styles/components/LoadingSpinner.styles.tsx
+++ src/styles/components/LoadingSpinner.styles.tsx
@@ -1,4 +1,3 @@
-import { mainColors } from "@utils/colors";
 import styled, { keyframes } from "styled-components";
 
 export const rotateAnimation = keyframes`
@@ -15,9 +14,9 @@ export const SpinnerContainer = styled.div`
   justify-content: center;
   align-items: center;
 
-  width: 50px;
-  height: 50px;
-  border-radius: 10px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
 
   position: sticky;
   top: 4px;
@@ -27,15 +26,15 @@ export const SpinnerContainer = styled.div`
   background-color: white;
   z-index: 2;
 
+  animation: ${rotateAnimation} 1s linear infinite;
+
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
 `;
 
-export const Spinner = styled.div<{ size?: number }>`
-  width: ${({ size }) => `${size || 40}px`};
-  height: ${({ size }) => `${size || 40}px`};
-  border-radius: 50%;
-  border: 3px solid ${mainColors.primary};
-  border-top-color: transparent;
-  border-right-color: transparent;
-  animation: ${rotateAnimation} 1s linear infinite;
+export const SpinningLogo = styled.img`
+  width: 30px;
+  height: 30px;
+
+  margin-top: -3px;
+  margin-left: 2px;
 `;
